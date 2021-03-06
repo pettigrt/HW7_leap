763429a1a4181df7428708381f38c3dd4b0cafcd
Tyler Pettigrew <pettigrt@oregonstate.edu>
2021-03-05 16:47:23 -0800

    Added second test to ensure years divisible by 100 are not leap years. Test currently fails

--- test_leap_year.py
+++ test_leap_year.py
@@ -2,8 +2,13 @@ import unittest
 import leap_year
 
 class TestLeapYear(unittest.TestCase):
+    #Test to make sure multiples of 4 are leap years
     def test_leap_year1(self):
         self.assertEqual(True, leap_year.leap_year(2020))
+
+    #Test to ensure years divisible by 100 are not leap years
+    def test_leap_year2(self):
+        self.assertEqual(False, leap_year.leap_year(2100))
 
 
 if __name__ == "__main__":
