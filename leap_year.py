b12af297ba59dd6c0cf6b45e7edce419bc0b9293
Tyler Pettigrew <pettigrt@oregonstate.edu>
2021-03-05 16:45:07 -0800

    Implemented code to say multiples of 4 are leap years. Test 1 now passes

---
+++ leap_year.py
@@ -0,0 +1,15 @@
+def leap_year(x):
+    if x % 4 == 0:
+        return True
+
+#No error checking on input for leap year. Assume correct input
+if __name__ == "__main__":
+    year = input("Enter the year: ")
+    try:
+        if leap_year(int(year)):
+            print(year, "is a leap year")
+        elif not leap_year(int(year)):
+            print(year, "is not a leap year")
+    except ValueError:
+        print("Input is not in the correct format")
+        exit
